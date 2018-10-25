baf738ea758b8d695e4edb64d6e5215495a43064
cool-beetles <adrianna.kucinska@onet.eu>
2018-10-25 20:33:26 +0200

    Fix Gemfile + generate controller for Trainings

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,20 @@
+class TrainingsController < ApplicationController
+  def index
+    @trainings = Training.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get '/new' => 'trainings#new'
-  post '/new' => 'trainings#create'
-  resources :trainings
-
   root "trainings#index"
 end
