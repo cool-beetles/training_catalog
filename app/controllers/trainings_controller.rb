a002c11ac7b0d69d545521425767dfce79f5d09c
cool-beetles <adrianna.kucinska@onet.eu>
2018-10-26 15:19:53 +0200

    Add action in trainings controller

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -4,17 +4,40 @@ class TrainingsController < ApplicationController
   end
 
   def show
+    @training = Training.find(params[:id])
   end
 
   def new
+    @training = Training.new
   end
 
   def create
+    @training = trainings.build(params_training)
+   
+    if @training.save
+      redirect_to @training 
+    else
+      render :new
+    end
   end
 
   def edit
+    @training = Training.find(params[:id])
   end
 
   def update
+    @training = Training.find(params[:id])
+
+    if @training.update(params_training)
+      redirect_to @training
+    else
+      render :edit
+    end
+  end
+
+  private
+  
+  def params_training
+    params.require(:training).permit(:title, exercise_attributes: [:id, :title, :set, :repetition])
   end
 end
